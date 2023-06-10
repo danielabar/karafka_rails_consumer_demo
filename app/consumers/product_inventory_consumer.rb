5e3c19d638f36669eb1803bba18e52b85621864f
Daniela Baron <baron.daniela@gmail.com>
2023-06-10 07:45:04 -0400

    dispatch invalid messages to dlq

--- app/consumers/product_inventory_consumer.rb
+++ app/consumers/product_inventory_consumer.rb
@@ -1,11 +1,16 @@
 class ProductInventoryConsumer < ApplicationConsumer
   def consume
     messages.each do |message|
-      Rails.logger.info("ProductInventoryConsumer consuming: Topic: #{topic.name}, Message: #{message.payload}, Offset: #{message.offset}")
+      Rails.logger.info(
+        "ProductInventoryConsumer consuming Topic: #{topic.name}, \
+        Message: #{message.payload},\
+        Offset: #{message.offset}"
+      )
       service = UpdateProductInventoryService.new(message.payload)
-      Rails.logger.error("ProductInventoryConsumer message invalid: #{service.errors.join(', ')}") unless service.process
-      # Optionally could call mark_as_consumed here for manual offset management.
-      # In this case, we leave it to Karafka to auto-commit offsets for simplicity.
+      unless service.process
+        Rails.logger.error("ProductInventoryConsumer message invalid: #{service.errors.join(', ')}")
+        dispatch_to_dlq(message)
+      end
     end
   end
 end
