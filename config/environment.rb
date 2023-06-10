78befbc6b808672cb1493b7077261617f4d5ef2b
Daniela Baron <baron.daniela@gmail.com>
2023-06-10 07:44:44 -0400

    update logging format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# https://stackify.com/rails-logger-and-rails-logging-best-practices/
+Rails.logger = Logger.new($stdout)
+Rails.logger.level = Logger::DEBUG
+Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
